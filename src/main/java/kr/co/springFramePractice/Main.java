e4c31a70e213654ac75cde0b9b20d09a87de0dc3
hajune <hajune67@smartix.co.kr>
2020-02-14 11:33:50 +0900

    spring DB test

--- src/main/java/kr/co/springFramePractice/Main.java
+++ src/main/java/kr/co/springFramePractice/Main.java
@@ -8,48 +8,38 @@ import java.sql.*;
 public class Main {
     private static Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) throws SQLException {
-        System.out.println("안녕하세요 테스트입니다.");
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         logger.info("로그를 찍어봅니다.");
 
-        Connection connection = null;
-        Statement statement = null;
-        try {
-            Class.forName("org.h2.Driver");
-            String url = "jdbc:h2:mem:test;MODE=MySQL;";
-            connection = DriverManager.getConnection(url, "sa", "");
-            statement = connection.createStatement();
+        Class.forName("org.h2.Driver");
+        String url = "jdbc:h2:mem:test;MODE=MySQL;";
 
-            //connection.setAutoCommit(false);
+        //JDK10 이상은 var 지원,  Connection => var
+        try(Connection connection = DriverManager.getConnection(url, "sa", "");
+            Statement statement = connection.createStatement()) {
 
+            connection.setAutoCommit(false);
             statement.execute("create table member(id int auto_increment, username varchar(255) not null, password varchar(255) not null, primary key(id))");
-            statement.executeUpdate("insert into member(username, password) values('DanB', '1234')");
 
+            try {
+                statement.executeUpdate("insert into member(username, password) values('DanB', '1234')");
+                connection.commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                connection.rollback();
+            }
+
+            //JDK10 이상은 var 지원,  ResultSet => var
             ResultSet resultSet = statement.executeQuery("select id, username, password from member;");
             while (resultSet.next()) {
-                int id = resultSet.getInt("ID");
-                String username = resultSet.getString("USERNAME");
-                String pw = resultSet.getString("PASSWORD");
+                Member member = new Member(resultSet); //Member => var
 
-                logger.info("sqlTest : /Debug MSG ==> ID : " + id + " userName : " + username + " passWord : " + pw);
+                //기존의 로그로 찍었던 것을 Member 에서 메소드로 정의해서 출력하도록 변경.
+                /*logger.info("sqlTest : /Debug MSG ==> ID : " + id + " userName : " + username + " passWord : " + pw);*/
+                logger.info(member.toString());
             }
-            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
-            connection.rollback();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                statement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
     }
 
